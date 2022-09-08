244fe92039e1e72ed01b34f98854bd27019486f5
Ayo Adekunle <ay4eva2006@gmail.com>
2022-09-08 05:43:03 -0700

    standard output with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("%s","with proper grammer, but the outcome is a peice of art,\n");
+	return (0);
+}
