aa4366ac346d1977fb47e30c77e9b186446fca47
Ayo Adekunle <ay4eva2006@gmail.com>
2022-09-08 03:32:17 -0700

    Standard output using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"with proper grammer, but the outcome is a peiceof art,");
+	return (0);
+}
