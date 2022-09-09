c4e616e70e12b1041b39358e662d125416efb956
Ayo Adekunle <ay4eva2006@gmail.com>
2022-09-09 13:58:13 -0700

    hot fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int a = '0';
-	
-	 for (a = '0'; a <= '9'; a++)
-	 {
-		 putchar(a);
-	 }
-	 putchar('\n');
-	 return (0);
+
+	   for (a = '0'; a <= '9'; a++)
+	   {
+		   putchar(a);
+	   }
+	   putchar('\n');
+	   return (0);
 }
