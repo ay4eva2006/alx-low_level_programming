4be37d3d15470037ec5e3c19b97964fbb1894928
Ayo Adekunle <ay4eva2006@gmail.com>
2022-09-10 01:32:23 -0700

    Hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	int i, j;
-	
-	for (i = 0; j < 1000; i++)
+
+	for (i = 0; j < 100; i++)
 	{
-		for (j = 0; j < 1000; j++)
+		for (j = 0; j < 100; j++)
 		{
 			if (i < j)
 			{
@@ -21,7 +21,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 988 || j != 999)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
