99118e71b6ca0103acfdf59eea880cc2ad7c052d
Ayo Adekunle <ay4eva2006@gmail.com>
2022-09-13 13:10:48 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include "main.h"
-
+  
 /**
- * main - print_alphabet
- * Return: 0
+ *  print_alphabet - Entry point
+ *
+ * Description: program that prints the alphabets
+ * followed by a new line
+ *
+ * Return: Exit int function with an int 0
  */
-
 void print_alphabet(void)
 {
-	char c = 'a';
-	
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	
-	}
-	_putchar('\n');
+		int aj;
+		
+		for (aj = 'a'; aj <= 'z'; aj++)
+		{
+			_putchar(aj);
+		}
+		   _putchar('\n');
+
 }
