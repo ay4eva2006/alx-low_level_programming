986efb1773b26becf458b918c30285030338e723
ay4eva2006 <99172660+ay4eva2006@users.noreply.github.com>
2022-10-18 18:02:36 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 {
 
 	int i;
-	char j;
+	Int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
