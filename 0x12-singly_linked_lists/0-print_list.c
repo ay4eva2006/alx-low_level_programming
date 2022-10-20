46b359877be8d199b6340aea292b1a7692478825
Ayo John Adekunle <ay4eva2006@gmail.com>
2022-10-20 12:24:12 -0700

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_list - prints list.
  * @h: param list
@@ -11,14 +10,13 @@ size_t print_list(const list_t *h)
 	size_t l_node;
 
 	l_node = 0;
-	while (h != NULL)
+	for (; h != NULL; l_node++)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		l_node++;
 	}
 	return (l_node);
 }
