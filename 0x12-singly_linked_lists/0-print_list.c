6474787ed2805180254d3d399d7f6f7abfa315d3
Ayo John Adekunle <ay4eva2006@gmail.com>
2022-10-20 13:08:50 -0700

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@ size_t print_list(const list_t *h)
 	size_t l_node;
 	
 	l_node = 0;
-	while (h != NULL)
+	while (*h != NULL)
 	{
-		if (h->str == NULL)
+		if (*h->str == NULL)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
@@ -21,7 +21,7 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
-		h = h->next;
+		*h = *h->next;
 		l_node++;
 	}
 	return (l_node);
